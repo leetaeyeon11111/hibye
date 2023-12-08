112026063dc42d0f817f0764f11c262cdec0026c
taeyeon <hahaha2662@naver.com>
2023-12-08 22:43:48 +0900

    7th

--- app/components/iyoiyo/TopicsList.jsx
+++ app/components/iyoiyo/TopicsList.jsx
@@ -4,6 +4,7 @@ import { HiPencilAlt } from 'react-icons/hi'
 import React from 'react'
 
 const getTopics = async () => {
+  const apiUrl = process.env.API_URL
   try {
     const res = await fetch(`${apiUrl}/api/topics`, {
       cache: 'no-store',
