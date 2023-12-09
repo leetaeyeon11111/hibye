f78b9260d4e69ccaa695ab763c9126c6b7bf4d42
taeyeon <hahaha2662@naver.com>
2023-12-09 14:56:43 +0900

    7th

--- app/editTopic/[id]/page.jsx
+++ app/editTopic/[id]/page.jsx
@@ -3,7 +3,7 @@ const getTopicById = async (id) => {
   const apiUrl = process.env.API_URL
 
   try {
-    const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+    const res = await fetch(`${apiUrl}/api/topics/${id}`, {
       cache: 'no-store',
     })
     if (!res.ok) {
